e1c39c33b381c7f606511589727f1c99607da751
googlehoosier <hoosier.daddy69xxx@gmail.com>
2025-07-17 00:44:09 +0530

    Add additional charges text to booking summary

---
+++ src/components/BookingSummary.tsx
@@ -0,0 +1,10 @@
+@@ .. @@
+             <p className="text-xs text-gray-400 mt-2">
+               Balance amount to be paid at venue
+             </p>
++            <p className="text-xs text-gray-400">
++              (Additional charges will be applied for guests exceeding the included limit, as per the respective venue.)
++            </p>
+           </div>
+         </div>
+       </div>
